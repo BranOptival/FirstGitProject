b5b928940fee307c10a9a3c0a4d831b421be9552
BranOptival <bran@optival.com>
2022-08-31 17:08:30 +0300

    Optival-Test

--- optivalDemo.py
+++ optivalDemo.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 dt_string = datetime.now()
-current_time= dt_string.strftime("%d/%m/%Y %H:%M:%S")
+current_time = dt_string.strftime("%d/%m/%Y %H:%M:%S")
 
 print(current_time)
